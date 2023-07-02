4fe08d996824fe980b357019fbba15e4521b9e5a
bonjugi <dev.bonjugi@gmail.com>
2023-07-02 18:57:10 +0900

    add health check

--- src/main/java/com/example/action/ActionApplication.java
+++ src/main/java/com/example/action/ActionApplication.java
@@ -8,7 +8,7 @@ public class ActionApplication {
 
     public static void main(String[] args) throws InterruptedException {
         System.out.println("10초 지연후 실행하기");
-        Thread.sleep(10000);
+        Thread.sleep(30_000);
         System.out.println("실행합니다");
         SpringApplication.run(ActionApplication.class, args);
     }
