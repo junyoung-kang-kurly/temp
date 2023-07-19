d1b6c438e3b2bcb12933719e7d6ea46dede029a2
bonjugi <dev.bonjugi@gmail.com>
2023-07-19 09:10:07 +0900

    client url

--- src/main/java/com/example/action/Controller.java
+++ src/main/java/com/example/action/Controller.java
@@ -1,22 +1,16 @@
 package com.example.action;
 
 import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-
-    @PostConstruct
-    public void delay() throws InterruptedException {
-        System.out.println("20초 지연후 실행하기");
-        Thread.sleep(20_000);
-        System.out.println("실행합니다");
-    }
-
     @GetMapping("/ping")
-    public ResponseEntity<String> ping() {
-        return ResponseEntity.ok("response!");
+    public ResponseEntity<String> ping(HttpServletRequest httpServletRequest) {
+        return ResponseEntity.ok(httpServletRequest.getRemoteAddr());
     }
+
 }
